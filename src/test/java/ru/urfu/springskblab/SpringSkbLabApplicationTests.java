4aa876ba76d842d86482bdaa5b3397b3dcc8cccf
aleksandr <a.d.beznutrov@urfu.me>
2025-02-24 23:27:26 +0500

    project created

---
+++ src/test/java/ru/urfu/springskblab/SpringSkbLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.springskblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSkbLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
