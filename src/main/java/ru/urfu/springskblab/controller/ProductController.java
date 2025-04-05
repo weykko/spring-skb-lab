1c9d39f89a14d4cf92f938339560fc2e960a938f
aleksandr <a.d.beznutrov@urfu.me>
2025-04-05 22:46:42 +0500

    минорные изменения

--- src/main/java/ru/urfu/springskblab/controller/ProductController.java
+++ src/main/java/ru/urfu/springskblab/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.urfu.springskblab.dto.ProductRequest;
 import ru.urfu.springskblab.dto.ProductResponse;
+import ru.urfu.springskblab.exception.CustomBadGatewayException;
 import ru.urfu.springskblab.service.ProductService;
 
 @RequiredArgsConstructor
@@ -17,5 +18,10 @@ public class ProductController {
     @PostMapping
     public ProductResponse addProduct(@RequestBody ProductRequest productRequest) {
         return productService.addProduct(productRequest);
+    }
+
+    @GetMapping("/502")
+    public void get502Error() {
+        throw new CustomBadGatewayException("ERROR! \uD83D\uDEAB\uD83D\uDEAB\uD83D\uDEAB");
     }
 }
