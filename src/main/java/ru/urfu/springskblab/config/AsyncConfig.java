e2f69dda52e5eb9e46a63e8b46359e0b5420dad3
aleksandr <a.d.beznutrov@urfu.me>
2025-04-28 22:09:28 +0500

    замержен код из БД таска

---
+++ src/main/java/ru/urfu/springskblab/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package ru.urfu.springskblab.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+}
